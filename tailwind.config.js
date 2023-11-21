8137047f0ebc4c0eeaebfdc148fbe5b52bb5d60a
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-21 12:38:05 -0300

    chore: add autoconf theme fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,23 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
     ],
 
     theme: {
+        colors: {
+            "autoconf-blue": "#52c508",
+            "autoconf-orange": "#EB9036",
+        },
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Rubik", ...defaultTheme.fontFamily.sans],
             },
         },
     },
