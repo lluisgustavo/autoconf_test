9b9ddd64cd0840e2ec4e882e9652db91da7b12de
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-22 05:49:08 -0300

    chore (tailwind): proper theme setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: "class",
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
@@ -11,12 +12,20 @@ export default {
     ],
 
     theme: {
-        colors: {
-            "autoconf-blue": "#0852C5",
-            "autoconf-orange": "#EB9036",
-            "autoconf-lightblue": "#E8F0FD",
-        },
         extend: {
+            colors: {
+                autoconf: {
+                    blue: "#0852C5",
+                    orange: "#EB9036",
+                    lightblue: "#E8F0FD",
+                },
+                dark: {
+                    "eval-0": "#151823",
+                    "eval-1": "#222738",
+                    "eval-2": "#2A2F42",
+                    "eval-3": "#2C3142",
+                },
+            },
             fontFamily: {
                 sans: ["Rubik", ...defaultTheme.fontFamily.sans],
             },
