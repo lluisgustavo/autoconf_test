200d01015063b7c82ee5641fc9e2b43b5e9bc846
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-23 07:55:12 -0300

    refactor (services): MakeService pint style

--- app/Services/MakeService.php
+++ app/Services/MakeService.php
@@ -1,9 +1,8 @@
-<?php 
+<?php
 
 namespace App\Services;
 
 use App\Repositories\MakeRepository;
-use App\Models\Make;
 
 class MakeService
 {
@@ -15,16 +14,17 @@ class MakeService
     }
 
     public function createMake(array $data)
-    { 
+    {
         return $this->makeRepository->create($data);
     }
+
     public function updateMake(int $id, array $data)
-    { 
+    {
         return $this->makeRepository->update($id, $data);
     }
 
     public function deleteMake(int $id)
-    { 
+    {
         return $this->makeRepository->delete($id);
-    } 
+    }
 }
