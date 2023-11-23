6d9f34453e999814107f3397e78757f941003ab5
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-22 21:51:26 -0300

    feat (vehicles): migration

---
+++ database/migrations/2023_11_21_180152_create_vehicles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('make_id')->constrained();
+            $table->foreignId('vehicle_model_id')->constrained();
+            $table->decimal('price', 10, 2);
+            $table->string('image_path');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
