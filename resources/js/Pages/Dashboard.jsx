6ab4c114fb46056d6be104037d3359ac4faf38db
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-22 21:50:19 -0300

    feat (layout): breeze basic layout

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,21 +1,23 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
 
-export default function Dashboard({ auth }) {
+export default (props) => {
     return (
         <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        >
-            <Head title="Dashboard" />
-
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
-                    </div>
+            title="Dashboard"
+            auth={props.auth}
+            errors={props.errors}
+            header={
+                <div className="flex flex-col text-gray-800 gap-4 md:flex-row md:items-center md:justify-between dark:text-white">
+                    <h2 className="text-xl font-semibold leading-tight">
+                        Dashboard
+                    </h2>
                 </div>
+            }
+        >
+            <div className="p-6 overflow-hidden bg-autoconf-lightblue rounded-md shadow-md dark:bg-dark-eval-1">
+                Bem vindo.
             </div>
         </AuthenticatedLayout>
     );
-}
+};
