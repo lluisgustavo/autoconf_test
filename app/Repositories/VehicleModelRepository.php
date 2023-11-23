2b0408610b15ec08014653b2082df1bfebc0cb77
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-23 07:40:31 -0300

    refactor (repositories): VehicleModelRepository pint style

--- app/Repositories/VehicleModelRepository.php
+++ app/Repositories/VehicleModelRepository.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
-use App\Models\VehicleModel;  
+use App\Models\VehicleModel;
 use Illuminate\Contracts\Pagination\Paginator;
 
 class VehicleModelRepository extends AbstractRepository
@@ -15,23 +15,23 @@ class VehicleModelRepository extends AbstractRepository
     {
         return $this->loadModel()::paginate($perPage);
     }
- 
+
     public function orderByPaginated($column = 'id', $direction = 'asc', $perPage = 10): Paginator
     {
         if ($column == 'make') {
             return $this->loadModel()::with('make')
                 ->join('makes', 'makes.id', '=', 'vehicle_models.make_id')
-                ->orderBy("makes.name", $direction)
-                ->select('vehicle_models.*') 
+                ->orderBy('makes.name', $direction)
+                ->select('vehicle_models.*')
                 ->paginate($perPage);
-        } 
-    
+        }
+
         return $this->loadModel()::with('make')->orderBy($column, $direction)->paginate($perPage);
     }
 
     public function getOptions()
     {
-        $vehicleModels = $this->loadModel()::all(); 
+        $vehicleModels = $this->loadModel()::all();
 
         $options = $vehicleModels->map(function ($vehicleModel) {
             return [
