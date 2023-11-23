b6cdc11258cc479c6b19f96287f2e26e4f6107cc
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-23 07:40:14 -0300

    refactor (repositories): VehicleRepository pint style

--- app/Repositories/VehicleRepository.php
+++ app/Repositories/VehicleRepository.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
-use App\Models\Vehicle;  
-use Illuminate\Contracts\Pagination\Paginator;  
+use App\Models\Vehicle;
+use Illuminate\Contracts\Pagination\Paginator;
 
 class VehicleRepository extends AbstractRepository
 {
@@ -15,24 +15,24 @@ class VehicleRepository extends AbstractRepository
     {
         return $this->loadModel()::paginate($perPage);
     }
- 
+
     public function orderByPaginated($column = 'id', $direction = 'asc', $perPage = 10): Paginator
-    {  
+    {
         if ($column == 'make') {
             return $this->loadModel()::with(['make', 'model'])
                 ->join('makes', 'makes.id', '=', 'vehicles.make_id')
-                ->orderBy("makes.name", $direction)
-                ->select('vehicles.*') 
+                ->orderBy('makes.name', $direction)
+                ->select('vehicles.*')
                 ->paginate($perPage);
-        } 
+        }
 
         if ($column == 'model') {
             return $this->loadModel()::with(['make', 'model'])
                 ->join('vehicle_models', 'vehicle_models.id', '=', 'vehicles.vehicle_model_id')
-                ->orderBy("vehicle_models.name", $direction)
-                ->select('vehicles.*') 
+                ->orderBy('vehicle_models.name', $direction)
+                ->select('vehicles.*')
                 ->paginate($perPage);
-        } 
+        }
 
         return $this->loadModel()::with(['make', 'model'])->orderBy($column, $direction)->paginate($perPage);
     }
