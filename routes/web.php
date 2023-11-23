ecf196106f19a7829272d32e38f83960079980bc
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-22 21:48:54 -0300

    chores (routes): routes for make, models and vehicles

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
+use App\Http\Controllers\MakeController;
+use App\Http\Controllers\VehicleModelController;
+use App\Http\Controllers\VehicleController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -15,14 +18,25 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */  
-
-Route::middleware('auth')->group(function () { 
+ 
+Route::middleware(['guest'])->group(function () {
     Route::get('/', function () {
-        return Inertia::render('Dashboard'); 
-    })->name('dashboard');
+        return redirect()->route('login');
+    });
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {  
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy'); 
+
+    Route::resource('marcas', MakeController::class)->except('show');
+    Route::resource('modelos', VehicleModelController::class)->except('show');
+    Route::resource('veiculos', VehicleController::class)->except('show');
+}); 
 
 require __DIR__.'/auth.php';
