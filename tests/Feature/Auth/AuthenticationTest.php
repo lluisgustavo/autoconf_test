2525170518c564b9a5c976c8468bde5060ac4365
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-23 07:37:41 -0300

    refactor (testing): AuthenticationTest pint style

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -21,13 +21,13 @@ class AuthenticationTest extends TestCase
     public function test_users_can_authenticate_using_the_login_screen(): void
     {
         $user = User::factory()->create();
- 
+
         $response = $this->post('/login', [
             '_token' => csrf_token(),
             'email' => $user->email,
             'password' => 'password',
         ]);
- 
+
         $this->assertAuthenticated();
         $response->assertRedirect(RouteServiceProvider::HOME);
     }
