f1adf2c6eb92d0ef29f8ecb21ed4af688fff9d40
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-22 21:42:49 -0300

    feat (make): service

---
+++ app/Services/MakeService.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace App\Services;
+
+use App\Repositories\MakeRepository;
+use App\Models\Make;
+
+class MakeService
+{
+    private $makeRepository;
+
+    public function __construct(MakeRepository $makeRepository)
+    {
+        $this->makeRepository = $makeRepository;
+    }
+
+    public function createMake(array $data)
+    { 
+        return $this->makeRepository->create($data);
+    }
+    public function updateMake(int $id, array $data)
+    { 
+        return $this->makeRepository->update($id, $data);
+    }
+
+    public function deleteMake(int $id)
+    { 
+        return $this->makeRepository->delete($id);
+    } 
+}
