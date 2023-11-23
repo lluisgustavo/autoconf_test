4e3ba23e5e5b2093ad8edc052c1def02ac30423f
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-23 01:39:11 -0300

    feat (factories): vehicle model factory

---
+++ database/factories/VehicleModelFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\VehicleModel>
+ */
+class VehicleModelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $makesIDs = DB::table('makes')->pluck('id');
+ 
+        return [
+            'id' => fake()->unique()->randomNumber(),
+            'make_id' => fake()->randomElement($makesIDs),
+            'name' => fake()->unique()->name(),
+            'manufacturing_year' => fake()->numberBetween(1800, 2023),
+            'fuel_type' => fake()->randomElement(['Gasolina', 'Diesel', 'Elétrico', 'Híbrido', 'GNV', 'Flex', 'Etanol', 'Outros']),
+            'type' => fake()->randomElement(['Carro', 'Motocicleta', 'Caminhão', 'Outro']),
+        ];
+    }
+}
