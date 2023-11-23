ae783dc861c34939329255d433634ddb7bc393d5
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-23 07:51:06 -0300

    refactor (database): seeder pint style

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {  
+    {
         \App\Models\User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
