18e8c0c1b6e044b5e5b7ed20b4cca594a4c7d80b
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-22 21:47:50 -0300

    feat (vehicle model): service

---
+++ app/Services/VehicleModelService.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace App\Services;
+
+use App\Repositories\VehicleModelRepository;
+use App\Models\VehicleModel;
+
+class VehicleModelService
+{
+    private $vehicleModelRepository;
+
+    public function __construct(VehicleModelRepository $vehicleModelRepository)
+    {
+        $this->vehicleModelRepository = $vehicleModelRepository;
+    }
+
+    public function createVehicleModel(array $data)
+    { 
+        return $this->vehicleModelRepository->create($data);
+    }
+    public function updateVehicleModel(int $id, array $data)
+    { 
+        return $this->vehicleModelRepository->update($id, $data);
+    }
+
+    public function deleteVehicleModel(int $id)
+    { 
+        return $this->vehicleModelRepository->delete($id);
+    }
+ 
+}
