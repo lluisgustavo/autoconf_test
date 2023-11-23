fb1fd674e773b44cde3b2163ae7e1ef4d9cca3f1
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-23 07:40:21 -0300

    refactor (repositories): MakeRepository pint style

--- app/Repositories/MakeRepository.php
+++ app/Repositories/MakeRepository.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
-use App\Models\Make;  
+use App\Models\Make;
 use Illuminate\Contracts\Pagination\Paginator;
 
 class MakeRepository extends AbstractRepository
@@ -15,14 +15,15 @@ class MakeRepository extends AbstractRepository
     {
         return $this->loadModel()::paginate($perPage);
     }
- 
+
     public function orderByPaginated($column = 'id', $direction = 'asc', $perPage = 10): Paginator
     {
         return $this->loadModel()::orderBy($column, $direction)->paginate($perPage);
     }
+
     public function getOptions()
     {
-        $makes = $this->loadModel()::all(); 
+        $makes = $this->loadModel()::all();
 
         $options = $makes->map(function ($make) {
             return [
