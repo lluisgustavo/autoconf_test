07ceda02ca10a207f2828f2b5ea27cf8af354123
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-23 07:36:30 -0300

    refactor (interfaces): pint style

--- app/Interfaces/RepositoryInterface.php
+++ app/Interfaces/RepositoryInterface.php
@@ -9,10 +9,15 @@ use Illuminate\Database\Eloquent\Model;
 
 interface RepositoryInterface
 {
-    public static function all():Collection;
-    public static function create(array $attributes):Model|null;
-    public static function find(int $id):Model|null;
-    public static function delete(int $id):int;
-    public static function update(int $id, array $attributes):int;
-    public static function loadModel():Model;
+    public static function all(): Collection;
+
+    public static function create(array $attributes): ?Model;
+
+    public static function find(int $id): ?Model;
+
+    public static function delete(int $id): int;
+
+    public static function update(int $id, array $attributes): int;
+
+    public static function loadModel(): Model;
 }
