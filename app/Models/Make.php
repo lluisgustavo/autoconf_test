c7df8d581c269242ea32651f60789148e11720bf
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-23 07:51:26 -0300

    fix (makes): remove soft deletes from model

--- app/Models/Make.php
+++ app/Models/Make.php
@@ -4,13 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-
 
 class Make extends Model
 {
     use HasFactory;
-    use SoftDeletes;
 
     protected $fillable = [
         'name',
@@ -18,11 +15,12 @@ class Make extends Model
         'founding_year',
         'country_of_origin',
     ];
+
     public function vehicles()
     {
         return $this->hasMany(Vehicle::class, 'make_id');
     }
-    
+
     public function models()
     {
         return $this->hasMany(VehicleModel::class, 'make_id');
