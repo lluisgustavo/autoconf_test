52d5bb930242cca7502b805c660189560a66f612
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-23 07:09:05 -0300

    refactor (routes): pint style

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use App\Http\Controllers\MakeController;
-use App\Http\Controllers\VehicleModelController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\VehicleController;
+use App\Http\Controllers\VehicleModelController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -17,8 +17,8 @@ use Inertia\Inertia;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/  
- 
+*/
+
 Route::middleware(['guest'])->group(function () {
     Route::get('/', function () {
         return redirect()->route('login');
@@ -29,14 +29,14 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {  
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy'); 
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('marcas', MakeController::class)->except('show');
     Route::resource('modelos', VehicleModelController::class)->except('show');
     Route::resource('veiculos', VehicleController::class)->except('show');
-}); 
+});
 
 require __DIR__.'/auth.php';
