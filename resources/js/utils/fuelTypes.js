d6dba21b161e1adb149d5c1e70e74fa24bddb6fb
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-22 21:49:16 -0300

    feat (utils): fuel types

---
+++ resources/js/utils/fuelTypes.js
@@ -0,0 +1,10 @@
+export const fuelTypeOptions = [
+    { label: "Gasolina", value: "Gasolina" },
+    { label: "Diesel", value: "Diesel" },
+    { label: "Elétrico", value: "Elétrico" },
+    { label: "Híbrido", value: "Híbrido" },
+    { label: "GNV", value: "GNV" },
+    { label: "Flex", value: "Flex" },
+    { label: "Etanol", value: "Etanol" },
+    { label: "Outro", value: "Outro" },
+];
