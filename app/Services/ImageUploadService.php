661521a8afe7a246ace75407a07b84f8723a5a9e
lluisgustavo <lluisgusttavo@gmail.com>
2023-11-23 07:54:58 -0300

    refactor (services): ImageUploadService pint style

--- app/Services/ImageUploadService.php
+++ app/Services/ImageUploadService.php
@@ -2,32 +2,32 @@
 
 namespace App\Services;
 
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use InvalidArgumentException;
 
 class ImageUploadService
 {
     public function uploadImage(UploadedFile $file)
-    { 
-        if (!$this->isImageValid($file)) {
+    {
+        if (! $this->isImageValid($file)) {
             throw new InvalidArgumentException('Invalid file.');
         }
- 
-        $filename = 'vehicle_images/' . uniqid() . '_' . $file->getClientOriginalName();
- 
+
+        $filename = 'vehicle_images/'.uniqid().'_'.$file->getClientOriginalName();
+
         Storage::disk('public')->put($filename, file_get_contents($file));
- 
+
         $path = Storage::url($filename);
-        
+
         return $path;
     }
 
     private function isImageValid(UploadedFile $file)
     {
         $allowedMimeTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'image/webp'];
-         
-        if (!in_array($file->getMimeType(), $allowedMimeTypes)) {
+
+        if (! in_array($file->getMimeType(), $allowedMimeTypes)) {
             return false;
         }
 
